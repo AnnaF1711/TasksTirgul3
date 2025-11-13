6f54e117ce979adefc6fbbaa9645d936e8da9858
Anna <anna.fyodorov@gmail.com>
2025-11-13 14:05:59 +0200

    1

---
+++ first/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import ClientData from './ClientData'
+
+function App() {
+
+
+  return (
+         <>
+       <div>
+           Client list:
+       </div>
+             <div>
+                 <ClientData/>
+             </div>
+         </>
+  )
+}
+
+export default App
